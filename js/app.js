36a7a3f29f3b98394f55f2da90cd1f131162656f
Todd Wolden <tcwolden@gmail.com>
2022-10-10 19:37:58 -0600

    cleaned up some spacing in js and html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-
-
-
-
 let hoursDaily = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+// Seattle
 
 let seattleList = document.getElementById('seattleList');
 console.log(seattleList);
@@ -36,7 +34,7 @@ for (let i=0; i < hoursDaily.length; i++) {
 }
 console.log(seattle);
 
-// toyko
+// Toyko
 
 let tokyoList = document.getElementById('tokyoList');
 let tokyo = {
